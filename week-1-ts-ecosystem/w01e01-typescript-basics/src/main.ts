d5ebb2f9f8a22f66b9376bd75d230d6b26845c40
P4NTY <49764816+P4NTY@users.noreply.github.com>
2022-09-28 09:23:42 +0200

    Zadanie 1

--- week-1-ts-ecosystem/w01e01-typescript-basics/src/main.ts
+++ week-1-ts-ecosystem/w01e01-typescript-basics/src/main.ts
@@ -1,12 +1,6 @@
-/**
- * Zasady co do pliku:
- *
- * Możesz dowolnie modyfikować zawartość tego pliku,
- * całość programu musi jednak działać tak jak do tej pory !
- *
- * */
+import Person from "./person";
 
-const user: any = {
+const user = new Person({
   name: 'Andy',
   age: 30,
   email: 'andy@mail-me-tommorow.com',
@@ -14,17 +8,7 @@ const user: any = {
     street: 'Strange Alley',
     no: 23,
   },
-}
+});
 
-function hasAddress(user: any): any {
-  return Boolean(user.address)
-}
-
-function hasGivenAge(requiredAge: any): any {
-  return (user: any): any => user.age >= requiredAge
-}
-
-const isAdult = hasGivenAge(18)
-
-console.log(`User ${user.name} is ${isAdult(user) ? 'adult' : 'minor'}`)
-console.log(`and has${hasAddress(user) ? '' : ' no'} address`)
+console.log(`User ${user.getName()} is ${user.hasGivenAge() ? 'adult' : 'minor'}`);
+console.log(`and has${user.hasAddress() ? '' : ' no'} address`);
